bb077d49da3c83d2485106c5330db34ff5aff377
netizen112 <netizen112@gmail.com>
2023-10-03 11:52:06 +0200

    add dist/ directory

---
+++ bundlea9f0f9b34d318560bc1a.js
@@ -0,0 +1,14 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+// import printMe from "./todo.js";
+
+// printMe();
+
+console.log("I am called from index.jss");
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVuZGxlYTlmMGY5YjM0ZDMxODU2MGJjMWEuanMiLCJtYXBwaW5ncyI6Ijs7Ozs7QUFBQTs7QUFFQTs7QUFFQSIsInNvdXJjZXMiOlsid2VicGFjazovL3RvcC10b2RvLWxpc3QvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiLy8gaW1wb3J0IHByaW50TWUgZnJvbSBcIi4vdG9kby5qc1wiO1xuXG4vLyBwcmludE1lKCk7XG5cbmNvbnNvbGUubG9nKFwiSSBhbSBjYWxsZWQgZnJvbSBpbmRleC5qc3NcIik7XG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
